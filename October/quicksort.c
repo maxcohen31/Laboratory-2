21f1e9abee6ab317c172a0b319c6b79b0dc38a61
maxcohen31 <eman_93@hotmail.it>
2024-10-16 21:47:42 +0200

    adding quicksort

---
+++ October/quicksort.c
@@ -0,0 +1,68 @@
+#define _GNU_SOURCE
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+void swap(int *x, int *y)
+{
+    int tmp = *x;
+    *x = *y;
+    *y = tmp;
+}
+
+int partition(int a[], int p, int r)
+{
+    int pivot = a[p];
+    int i = p - 1;
+    int j = r + 1;
+
+    do 
+    {
+        do 
+        {
+            j--; // decreasing j since the pivot is lesser than a[j]
+        } 
+        while (a[j] > pivot);
+
+        do 
+        {
+            i++; // increasing i since a[i] is lesser than a[i]
+        }
+        while (a[i] < pivot);
+        
+        if (i < j)
+        {
+            swap(&a[i], &a[j]);
+        }
+    } 
+    while (i < j); // main cycle
+
+    return j;
+}
+
+void quickSort(int arr[], int l, int r) {
+
+    if (l < r) 
+    {
+        int part = partition(arr, l, r);
+
+        quickSort(arr, l, part - 1);
+        quickSort(arr, part + 1, r);
+    }
+}
+
+
+int main(int argc, char *argv[])
+{
+    int arr[7] = {10, 4, 3, 6, 7, 9, 1};
+    int n = (sizeof(arr) / sizeof(arr[0]));
+
+    quickSort(arr, 0, n  - 1);
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
