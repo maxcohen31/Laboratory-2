3876a73c4076eaaced89679aa3ca4ea69eaa9c49
maxcohen31 <eman_93@hotmail.it>
2024-11-05 16:19:23 +0100

    feat: esercizio funzione abr_ricerca_range

--- November/abr_capitali.c
+++ November/abr_capitali.c
@@ -34,7 +34,7 @@ void capitale_stampa(const capitale *a, FILE *f)
 capitale *capitale_crea(char *s, double lat, double lon)
 {
     capitale *a = malloc(sizeof(*a));
-    a->nome = s;
+    a->nome = strdup(s);
     a->lat = lat;
     a->lon = lon;
     a->left = NULL;
@@ -66,11 +66,10 @@ void abr_capitale_distruggi(capitale *root)
     if (root != NULL)
     {
         abr_capitale_distruggi(root->left);
-        capitale_distruggi(root);
         abr_capitale_distruggi(root->right);
+        capitale_distruggi(root);
     }
 }
-
 
 // legge un oggetto capitale dal file f
 // restituisce il puntatore all'oggetto letto
@@ -154,23 +153,124 @@ capitale *crea_abr(FILE *f)
     return root;
 }
 
+capitale *ricerca_abr(capitale *root, char *nome)
+{
+    if (root == NULL)
+    {
+        return NULL;
+    }
+
+    int e = strcmp(root->nome, nome);
+
+    if (e == 0)
+    {
+        return root;
+    }
+    else if (e < 0)
+    {
+        return ricerca_abr(root->left, nome);
+    }
+    else 
+    {
+        return ricerca_abr(root->right, nome);
+    }
+}
+
+int abr_altezza(capitale *root)
+{
+    if (root == 0)
+    {
+        return -1;
+    }
+
+    int right_h = abr_altezza(root->right);
+    int left_h = abr_altezza(root->left);
+
+    return (right_h > left_h) ? right_h + 1 : left_h + 1;
+}
+
+// retituisce true se c ha latitudine tra 40 e 43
+bool latrange(capitale *c)
+{
+    assert(c!=NULL);
+    return (c->lat >= 40.0) && (c->lat <= 43.0);
+}
+
+void abr_stampa_cond(capitale *root, FILE *f, bool (*funz)(capitale *))
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    abr_stampa_cond(root->left, f, funz);
+    
+    if (funz(root))
+    {
+        capitale_stampa(root, f);
+    }
+
+    abr_stampa_cond(root->right, f, funz);
+
+}
+
+int abr_ricerca_range(capitale *root, char *smin, char *smax)
+{
+    if (root == NULL)
+    {
+        return 0;
+    }
+
+    int c1 = strcmp(root->nome, smin); // confronta nome e smin
+    int c2 = strcmp(root->nome, smax); // confronta nome e smax
+    int counter = 1; // nodo corrente
+
+    if (c1 >= 0 && c2 <= 0) // stampa nome nel range attuale
+    {
+        capitale_stampa(root, stdout);
+        puts("\n");
+    }
+
+    if (c1 > 0) // smin < nome -> sottoalbero sx
+    {
+        counter += abr_ricerca_range(root->left, smin, smax);
+    }
+
+    else if (c2 < 0) // smax > nome -> sottoalbero dx
+    {
+        counter += abr_ricerca_range(root->right, smin, smax);
+    }
+
+    return counter;
+
+
+}
+
 int main(int argc, char **argv)
 {   
-    if(argc!=2) 
+    if (argc != 4) 
     {
         printf("Uso: %s nomefile\n",argv[0]);
         exit(1);
     } 
 
     FILE *f = fopen(argv[1],"r");
-    if(f==NULL) termina("Errore apertura file");
+    if (f == NULL) 
+    {
+        termina("Errore apertura file");
+    }
 
     // costruzione albero leggendo capitali dal file
-    capitale *radice=crea_abr(f);
-    puts("--- inizio lista ---");
+    capitale *radice = crea_abr(f);
+    // puts("--- inizio lista ---");
     // stampa lista capitali appena creata
-    abr_capitale_stampa(radice,stdout);  
-    puts("--- fine lista ---");
+    // abr_capitale_stampa(radice,stdout);  
+    // puts("--- fine lista ---");
+
+    puts("Chiama abr_ricerca_range\n");
+    int n = abr_ricerca_range(radice, argv[2], argv[3]);
+    printf("range nomi esplorati: %d\n", n);
+
     abr_capitale_distruggi(radice);
 
     if (fclose(f) == EOF)
