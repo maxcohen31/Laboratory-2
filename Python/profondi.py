609eb4f74dc01547dade824a84e215b4ff0fca58
maxcohen31 <eman_93@hotmail.it>
2026-02-19 11:03:34 +0100

    profondi

--- Python/profondi.py
+++ Python/profondi.py
@@ -1,5 +1,4 @@
-from posixpath import realpath
-from sys import argv, stdlib_module_names
+from sys import argv, exit
 from os import R_OK, X_OK, F_OK, access
 from os import path
 from os import listdir
@@ -8,39 +7,64 @@ from sys import stderr
 
 def elenco_file(directory):
     def aux(current, visited, depth):
-        assert path.isdir(directory), "Argument is not a directory" 
+        assert path.isdir(current), "Argument is not a directory" 
         result = []
 
         # Content of our starting directory
         current_content = listdir(current) 
-
         for file in current_content:
             complete_name = path.join(current, file)
             # Is a file and it is accessable?
             if not access(complete_name, F_OK):
-                print(f"[Broken Link] {complete_name}", stderr)
+                print(f"[Broken Link] {complete_name}", file=stderr)
                 continue
             if not path.isdir(complete_name):
-                # file found and is accessable
+                # File found and is accessable
                 new_dim = path.getsize(complete_name)
                 new_name = complete_name
-
-                # get the real name of the directory and add it to visited
+                # Adding file to result
+                result.append((new_name, new_dim, depth))
+            else:
+                # Found a directory. Is it explorable?
+                if not access(complete_name, R_OK | X_OK):
+                    print(f"Directory {complete_name} not accessable")
+                    continue
+                # Get the absolute path of our directory
                 real_name = path.abspath(complete_name)
-                visited.add(real_name)
-
-
-
+                # Adding directory to visited
+                if real_name in visited:
+                    print(f"Directory {complete_name} already explored", file=stderr)
+                    continue
+                # add our current directory to visited
+                visited.append(real_name)
+                # New directory found: explore it 
+                explored = aux(complete_name, visited, depth + 1)
+                result += explored
+                print(f"End: {complete_name}", file=stderr)
         return result
-    aux(directory, [], 0)
+    return aux(directory, [], 0)
 
 
 
 def main():
     if len(argv) != 2:
         print("Usage: python3 <program_name> <dir_name>")
-    else:
-        elenco_file(argv[1])
+        exit(1)
+    if not path.isdir(argv[1]):
+        print("Argument is not a directory")
+        exit(2)
 
+    # absolute name
+    abs_name = argv[1]
+    res = elenco_file(abs_name)
+
+    print("******* LIST OF FILES ******* ")
+    #print(res)
+    depth = max([tup[2] for tup in res])
+    files_at_max_depth = [(tup[0], tup[1]) for tup in sorted(res, key=lambda x: x[1], reverse=True) if tup[2] == depth]
+    
+    for f,dim in files_at_max_depth:
+        print(f"{f} - Dimension: {dim}")
+    
 if __name__ == "__main__":
     main()
