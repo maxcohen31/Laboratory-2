d2eeeb9d4c633efe80b2ac4e04d5d2c762c6c669
maxcohen31 <eman_93@hotmail.it>
2026-04-08 10:25:04 +0200

    esercitazione 26/03/2026

--- March2026/ricbin.c
+++ March2026/ricbin.c
@@ -1,14 +1,13 @@
-#include <math.h>
+#define _GNU_SOURCE
 #include <stddef.h>
 #include <time.h>
-#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include <errno.h>
 #include <assert.h>
 #include <pthread.h>
 #include <semaphore.h>
+#include <unistd.h>
 
 /*
     Utilizzare il programma scrivi_primi_bin.c per creare un file binario contenente tutti i primi minori di 10000.
@@ -42,12 +41,14 @@
 #define BUF_SIZE 10
 #define CONS_THREAD 3
 
-/* Consumer struct */
+/* Consumer's struct */
 typedef struct 
 {
-    int *cindex;
+    int *cindex;           /* Current buffer position */
     int *buffer;
-    int *received;
+    int *primes;
+    int primes_dim;
+    int received;          /* number of primes encountered */
     long sum;
     pthread_mutex_t *mu;
     sem_t *empty;
@@ -88,10 +89,10 @@ bool find_prime(int *arr, int dim, int n)
 void *cons_thread(void *arg)
 {
     consumer_t *c = (consumer_t*)arg;
-    c->sum = 0;
-    c->received = 0;
+    long sum = 0;
+    int received = 0;
     int n = 0;
-    printf("[C] started\n");
+    printf("[C] Consumer %d has started\n", gettid() % 100);
 
     while (n != -1)
     {
@@ -102,18 +103,21 @@ void *cons_thread(void *arg)
         pthread_mutex_unlock(c->mu);
         sem_post(c->empty);
         
-        if (find_prime(c->buffer, BUF_SIZE, n))
+        if (find_prime(c->primes, c->primes_dim, n))
         { 
-            c->sum += n;
-            c->received += 1;
+            sum += n;
+            received += 1;
         }
     }
+    c->sum = sum;
+    c->received = received;
+    printf("[C] Consumer %d has finished\n with sum %ld\n", gettid() % 100, c->sum);
     pthread_exit(NULL);
 }
 
 int main(int argc, char **argv)
 {
-    if (argc != 2)
+    if (argc != 3)
     {
         fprintf(stderr, "Usage:\t%s <prime_file> <integers_file>", argv[0]);
         exit(EXIT_FAILURE);
@@ -146,32 +150,92 @@ int main(int argc, char **argv)
     }
 
     fclose(fp);
-    for (int i = 0; i < count; i++) printf("%d\n", a[i]);
-    
+
     /* Buffer */
     int buff[BUF_SIZE];
     int cindex = 0;
+    int pindex = 0;
     long result = 0;
+    int tot_primes = 0;
 
     /* Thread stuff */
     pthread_t thread_arr[CONS_THREAD];
     pthread_mutex_t mu = PTHREAD_MUTEX_INITIALIZER;
     sem_t full;
     sem_t empty;
-    sem_init(&full, BUF_SIZE, 0);
-    sem_init(&empty, 0, 0);
+    sem_init(&full, 0, 0);
+    sem_init(&empty, 0, BUF_SIZE);
+
     /* Array of consumer structs */
     consumer_t consumer_arr[CONS_THREAD];
     for (int i = 0; i < CONS_THREAD; i++)
     {
+        consumer_arr[i].primes = a;
+        consumer_arr[i].primes_dim = count;
         consumer_arr[i].cindex = &cindex;
+        consumer_arr[i].buffer = buff;
         consumer_arr[i].empty = &empty;
         consumer_arr[i].full = &full;
         consumer_arr[i].sum = result;
         consumer_arr[i].mu = &mu;
-        pthread_create(&thread_arr[i], NULL, cons_thread, consumer_arr + 1);
+        consumer_arr[i].received = tot_primes;
+        pthread_create(&thread_arr[i], NULL, cons_thread, consumer_arr + i);
     }
 
+    /* Working on argv[2] - argv[2] is not a binary file */
+    int n;
+    FILE *fp2 = fopen(argv[2], "r");
+    if (fp2 == NULL)
+    {
+        perror("Error opening file [argv2]");
+        exit(3);
+    }
+    /* Reading integers from the file */
+    while (true)
+    {
+        int e = fscanf(fp2, "%d", &n);
+        if (e != 1) break;
+        assert(n > 0);
+        sem_wait(&empty);
+        buff[pindex++ % BUF_SIZE] = n;
+        sem_post(&full);
+    }
+    printf("[P] Data has been written\n");
+    fclose(fp2);
+ 
 
+    /* Inserting the stop value into the buffer */
+    for (int i = 0; i < CONS_THREAD; i++)
+    {
+        sem_wait(&empty);
+        buff[pindex++ % BUF_SIZE] = -1;
+        sem_post(&full);
+    }
+
+    /* Join the threads */
+    for (int i = 0; i < CONS_THREAD; i++)
+    {
+        if (pthread_join(thread_arr[i], NULL) != 0)
+        {
+            perror("Error joining threads");
+            exit(3);
+        }
+    }
+
+    /* Calculate the sum */
+    for (int i = 0; i < CONS_THREAD; i++)
+    {
+        tot_primes += consumer_arr[i].received;
+        result += consumer_arr[i].sum;
+    }
+
+    printf("Primes encountered: %d\nSum: %ld\n", tot_primes, result);
+
+
+    sem_destroy(&full);
+    sem_destroy(&empty);
+    pthread_mutex_destroy(&mu);
+
+    printf("Total sum: %ld\n", result);
     return 0;
 }
